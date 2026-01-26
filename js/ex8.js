d5bdfd6cff3745bc9e57ca62ad6d2af1c0e628ba
Thiago Lemos <lemost45@gmail.com>
2026-01-26 12:20:39 -0300

    atualizando...

---
+++ js/ex8.js
@@ -0,0 +1,12 @@
+// Exsercício com array
+
+alert('Verificação de média');
+
+function calculo() {
+    let notas = [7, 8, 6, 9];
+    const media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+    return media;
+}
+alert(`O valor da sua média é: ${calculo()}`);
+
+// Imprimir array no console
---
+++ js/ex9.js
@@ -0,0 +1,19 @@
+// Imprimindo um array no console:
+
+const nomes = ['Thiago', 'Luiza', 'Antonio', 'Luiz', 'Laura', 'Bruna'];
+
+console.log(nomes);
+
+// Acessar posições específicas
+
+console.log(nomes[2]);
+console.log(nomes[0]);
+console.log(nomes[5]);
+
+// Mudando um valor
+
+nomes[0] = 'Lemos';
+nomes[3] = 'Ferreira';
+nomes[2] = 'Silva';
+
+console.log(nomes);
