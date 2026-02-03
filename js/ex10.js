41e569a82d338364c0369b4be3f97ea306f9a7f2
Thiago Lemos <lemost45@gmail.com>
2026-02-03 16:25:24 -0300

    atualizando...

---
+++ js/ex10.js
@@ -0,0 +1,21 @@
+// Contando de 1 até 10 com for
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log('<==>')
+
+// Tabuada de 2
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i * 2)
+}
+
+console.log('<==>')
+
+// Tabuada de 2 mostrando os valores na tela
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`2 x ${i} =`, i * 2)
+}
